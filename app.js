82302276f2b1274a89ab6498e63e148bac798acb
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-07-30 15:04:36 -0300

    Modulo 1 completo

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 alert('Boa vindas ao jogo do número secreto');
 
 let numeroSecreto = 5;
-prompt('Escolha um número entre 1 e 30')
+let chute = prompt('Escolha um número entre 1 e 30')
+
+if (chute == numeroSecreto){
+    console.log('Isso ai! Voce descobriu o numero secreto (5)');
+}
---
+++ exercicios/desafios.js
@@ -0,0 +1,19 @@
+alert('Boas vindas ao nosso site!')
+
+//let nome = lua;
+//let idade = 25;
+let nomeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos')
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+let nome = prompt('Informe seu nome:');
+
+let idade = prompt('Informe a sua idade:');
+
+if(idade >= 18){
+    alert('Pode tirar a habilitação!');
+}
