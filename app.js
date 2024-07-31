d7ff44fb4de3e4e5874f9600fb415f5697d1c6bc
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-07-31 20:34:07 -0300

    Modulo 3 aula 2 completo

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 alert('Boa vindas ao jogo do número secreto');
 
-let numeroSecreto = 4;
+let numeroSecreto = 3;
 //O programa já vai mostrar o numero secreto porem somente no console.
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 30')
+let chute 
 
-// Se o chute for igual ao numero secreto
-if (chute == numeroSecreto){
-    alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);
-}else {
-    alert('Você errou :(');
+//enquanto chute não for igual o numero secreto
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+        // Se o chute for igual ao numero secreto
+        if (chute == numeroSecreto){
+            alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);
+        } else {
+            if(chute > numeroSecreto){
+                alert(`O número secreto é menor que ${chute}.`);
+            }
+            else{
+                alert(`O número secreto é maior que ${chute}`);
+            }
+    }
+        /*
+        //Outra forma de fazer o if do numero secreto.
+        if (chute == numeroSecreto) {
+            alert('Acertou')
+        } else {
+            alert('O número secreto era ' + numeroSecreto)
+        }*/
 }
-/*
-Outra forma de fazer o if do numero secreto.
-if (chute == numeroSecreto) {
-    alert('Acertou')
-} else {
-    alert('O número secreto era ' + numeroSecreto)
-}*/
