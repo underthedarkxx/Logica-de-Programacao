af77ca11baeee363b3fb5ff34e7cc2061503c59d
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-07-31 10:43:10 -0300

    Modulo 2 aula 2 completa

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 alert('Boa vindas ao jogo do número secreto');
 
 let numeroSecreto = 5;
+//O programa já vai mostrar o numero secreto porem somente no console.
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 30')
 
+// Se o chte for igual ao numero secreto
 if (chute == numeroSecreto){
-    console.log('Isso ai! Voce descobriu o numero secreto (5)');
+    alert('Isso ai! Voce descobriu o numero secreto (5)');
+}else {
+    alert('Você errou :(');
 }
