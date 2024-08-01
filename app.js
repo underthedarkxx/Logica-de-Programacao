c6b99d846c44a63744e41bf4b207e6d42fb61c41
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-08-01 00:10:58 -0300

    capitulo 3 completo

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ let numeroSecreto = 3;
 //O programa já vai mostrar o numero secreto porem somente no console.
 console.log(numeroSecreto);
 let chute;
-
+let tentativas = 1;
 
 //enquanto chute não for igual o numero secreto
 while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 10');
         // Se o chute for igual ao numero secreto
         if (chute == numeroSecreto){
-            alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);
+            alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
         } else {
+
             if(chute > numeroSecreto){
                 alert(`O número secreto é menor que ${chute}.`);
             }
             else{
                 alert(`O número secreto é maior que ${chute}`);
             }
-    }
+            //tentativas = tentativas + 1;
+            tentativas++;
+        }
         /*
         //Outra forma de fazer o if do numero secreto.
         if (chute == numeroSecreto) {
