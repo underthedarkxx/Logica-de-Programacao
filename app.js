3b9e9148958167dc86b54444608b8a2bec95be40
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-08-03 12:17:48 -0300

    Curso completo de JS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Boa vindas ao jogo do número secreto');
-
+let numeroTotal = 3;
 //serve para gerar um numero aleatório que não sabemos. 
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroSecreto = parseInt(Math.random() * numeroTotal + 1);
 //O programa já vai mostrar o numero secreto porem somente no console.
 console.log(numeroSecreto);
 let chute;
@@ -9,7 +9,7 @@ let tentativas = 1;
 
 //enquanto chute não for igual o numero secreto
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroTotal}`);
         // Se o chute for igual ao numero secreto
         if (chute == numeroSecreto){
             //Para
