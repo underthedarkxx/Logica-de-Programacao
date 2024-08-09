2cd81b04de1180ec40cc5fb7e79ce51231e5ca32
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-08-09 09:36:53 -0300

    Modulo 2 + Exercicios completo

--- Segundo Curso/Curso 2/app.js
+++ Segundo Curso/Curso 2/app.js
@@ -1,11 +1,29 @@
 //Case sensitive= Diferencie maiusculas e minusculas.
 
-let titulo = document.querySelector/*Case sensitive*/('h1');
-titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let titulo = document.querySelector/*Case sensitive*/('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
 
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let numeroSecreto = gerarNumeroAleatorio();
+
+//funções com retorno e parametro.
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+//função sem retorno e parametro
 function verificarChute(){
-    console.log('O botão foi clicado!!');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+//função com retorno
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
--- Segundo Curso/Desafios/app.js
+++ Segundo Curso/Desafios/app.js
@@ -1,3 +1,5 @@
+/*
+Desafio modulo 1
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do Desafio';
 
@@ -21,3 +23,61 @@ function verificarSoma(){
     alert (`O numero ${numero1} + ${numero2} = ${resultado}`);
 }
 
+
+//Desafio modulo 2
+//Criar uma função que exibe "Olá, mundo!" no console.
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+/*Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo.*/
+/*
+function verificarConsole (){
+    console.log('Olá, mundo!');
+}
+*/
+/*
+function verificarConsole(nome){
+    console.log(`Olá, ${nome}!`);
+}
+verificarConsole('Roberto');
+*/
+/*
+function dobronum(num){
+    return num*2;
+}
+
+let exibirNum = dobronum(2);
+console.log(exibirNum);
+*/
+/*
+function mediaNum(num1, num2, num3) {
+    return (num1+num2+num3)/3;
+}
+let calcMed = mediaNum(3,3,3);
+console.log(calcMed);
+*/
+/*
+function maiorNum(num1, num2){
+    if(num1 > num2){
+        return num1;
+    }
+    else if(num2 > num1){
+        return num2;
+    }
+    else{
+        return ('Os dois numeros são iguais');
+    }
+}
+
+let recebendoNum = maiorNum(3, 2);
+console.log(recebendoNum);
+*/
+/*
+function potNum(num){
+    return num*num;
+}
+
+let reciveNum = potNum(10);
+console.log(reciveNum);
+*/
